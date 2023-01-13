1b04293e5146714251bcfa34f9c15f0dee4b3e29
lazaronazareno <lazaronazareno@gmail.com>
2023-01-13 04:58:14 -0300

    json server init, routing and axios config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import EditProduct from './components/EditProduct'
+import Header from './components/Header'
+import NewProduct from './components/NewProduct'
+import Products from './components/Products'
 
-function App() {
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Provider store={store}>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Products />} />
+          <Route exact path='/products/new' element={<NewProduct />} />
+          <Route exact path='/products/:id' element={<EditProduct />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
