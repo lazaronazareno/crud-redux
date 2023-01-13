7c3356d9c275a2efec7fe640568e137c6089e5e8
lazaronazareno <lazaronazareno@gmail.com>
2023-01-13 04:56:30 -0300

    upload image and header component

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import image from '../assets/background.png'
+
+const Header = () => {
+  return (
+    <nav className='navbar navbar-expand-lg justify-content-between relative p-0'>
+      <img
+        src={image}
+        alt='background figure, midjourney art'
+        className='header__img'
+      />
+      <h1 className='header__title text-dark'><Link to='/' className='text-dark text-decoration-none'>CRUD REDUX</Link></h1>
+
+      <Link to='/products/new' className='header__button btn btn-success'>&#43; Agregar Producto</Link>
+    </nav>
+  )
+}
+
+export default Header
